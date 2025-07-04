67a280ccd3951fbb720515d9d6b526ffb22af7e9
Jean-Marie Renouard <jmrenouard@gmail.com>
2025-07-04 10:39:58 +0200

    feat: Add MongoDB configuration and initialization scripts for sharded cluster setup

---
+++ docker/sharded/init-scripts/init-datadir.sh
@@ -0,0 +1,5 @@
+# Créer les répertoires nécessaires
+mkdir -p {init-scripts,config,data/keyfile}
+mkdir -p data/{config1,config2,config3}
+mkdir -p data/{shard1-1,shard1-2,shard1-3}
+mkdir -p data/{shard2-1,shard2-2,shard2-3}
